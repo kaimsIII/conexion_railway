a00721699163623e8b2677b35b5b00f6e652e953
kevin.dominich <kevin.dominich@netec.com.pe>
2025-04-25 14:48:09 -0500

    data base 2

--- backend/login_app/views.py
+++ backend/login_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# views.py
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+import os
 
-# Create your views here.
+@login_required
+def download_db(request):
+    db_path = os.getenv('DATABASE_PATH', '/login_app/data/db.sqlite3')
+    with open(db_path, 'rb') as f:
+        response = HttpResponse(f.read(), content_type='application/x-sqlite3')
+        response['Content-Disposition'] = 'attachment; filename=db.sqlite3'
+        return response
